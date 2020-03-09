160ab0bd15239670fa7ab5fa4d0a963d270d89ea
shinilt <shinilstar@gmail.com>
2020-03-09 15:56:30 +0100

    updated the code as per new design

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, Response
 from bs4 import BeautifulSoup
 import requests
 import numpy as np
@@ -27,12 +27,13 @@ class myThread (threading.Thread):
 
 
 app = Flask(__name__)
-htmltable = ""
+dataframejson = ""
 
 def get_new_data_every(period):
 
     while True:
-        GenerateResources()
+        global dataframejson
+        dataframejson = GenerateResources()
         time.sleep(period)
 
 def GenerateResources():
@@ -41,7 +42,7 @@ def GenerateResources():
     req_data = requests.get(URL)
     review_soup = BeautifulSoup(req_data.content, 'html.parser')
 
-    #copy only selected columns
+    #copy only required columns
     df2 = df[0].iloc[:, [0, 1, 2, 3, 5, 6]]
     #rename the columns
     df2.columns = ['Country Or Location', 'Total Cases', 'New Cases', 'Total Deaths', 'Total Recovered', 'Active Cases']
@@ -49,8 +50,8 @@ def GenerateResources():
     cols = ['Total Cases', 'Total Deaths', 'Total Recovered']
     df2 = df2.fillna(0)
     df2[cols] = df2[cols].fillna(0).applymap(np.int64)
-
-
+    """
+    #commenting the entire section as the plot and table generation moved to javascript
     #plot the bar chart of top 25 countries in logaarithmic scale
     plt.figure(figsize=(23, 10))
     plt.title('25 Countries with highest confirmed cases')
@@ -64,7 +65,7 @@ def GenerateResources():
     # colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
     # explode = (0.1, 0, 0, 0)  # explode 1st slice
 
-    # Plot the pie chart
+    # Plot the pie chart. 
     plt.figure(figsize=(14, 10))
     # plt.pie(Cases,   shadow=False, startangle=90)
     patches, texts = plt.pie(Cases, shadow=False, startangle=90)  # removed the percentage display autopct='%1.1f%%'
@@ -75,142 +76,35 @@ def GenerateResources():
     plt.savefig('static/PieChart2.png')
     #close all plots to save memory
     plt.close("all")
-
-    # write the dataframe html to file
+    # write the dataframe html to file. Heroku is not letting save the files. Not suing now as the design changed
     html = df2.to_html()
-    #create json from dataframe for the use in web javascript
-    dfJson = df2.to_json()
-    print(dfJson)
-    global htmltable
-    htmltable = html
     text_file = open("templates/CountryList.html", "w")
     text_file.write(html)
-    text_file.close()
+    text_file.close() 
+    """
+
+    #create json from dataframe for the use in web javascript
+    dfJson = df2.to_json()
     return dfJson
 
-# refresh the data , generate new data every 10 mins
-#schedule.every(10).minutes.do(GenerateResources)
 
-@app.route("/home", methods=['GET'])
-def home():
-    return render_template("home.html")
 
 
 @app.route("/", methods=['GET'])
-def getJson():
-    str1 = """
-    <!DOCTYPE html>
-<html>
-<head>
-    <title>Corona Virus Updates and Statistics</title>
-	<!-- Load plotly.js into the DOM -->
-	<script src='https://cdn.plot.ly/plotly-latest.min.js'></script>
-</head>
-<body>
-<h1>NCOVID-19 Information and statistics</h1>
-<h2>Cumulative cases</h2>
-<div id='BarChartAllDiv'>
-<h2>Active cases</h2>
-<div id='BarChartActiveDiv'>
-<h2>New cases</h2>
-<div id='BarChartNewCaseDiv'>
-<h2>Total Death stats</h2>
-<div id='BarChartTotalDeathsDiv'>
+def Index():
+    return render_template("index.html")
 
 
-<h2>Cumulative cases</h2>
-<div id='PieChartAllDiv'>
-<h2>Current Active cases</h2>
-<div id='PieChartActiveDiv'>
-<h2>New cases</h2>
-<div id='PieChartNewCaseDiv'>
-<h2>Total Deaths</h2>
-<div id='PieChartTotalDeathsDiv'>
-
-<h2>Complete list of affected areas</h2>
-<p id="AffectedCountryTable">Pulling out the dat from several places.</p>
-
-
-<div id='text1'>
-<div id='text2'>
-<script>
-var txt = """
-    str2 = GenerateResources()
-    str3 = """
-var obj = JSON.parse(txt);
-Country=[];
-TotalCases=[];
-ActiveCases=[];
-NewCases=[];
-TotalDeaths=[];
-txt = "<table border='1'><tr><th>Country Or Location</th><th>Total Cases</th><th>Total Deaths</th><th>Total Recovered</th><th>Active Cases</th></tr>"
-      for (x in obj['Country Or Location']) {
-      Country.push(obj['Country Or Location'][x]);
-      TotalCases.push(obj['Total Cases'][x]);
-      ActiveCases.push(obj['Active Cases'][x]);
-      NewCases.push(obj['New Cases'][x]);
-      TotalDeaths.push(obj['Total Deaths'][x]);
-        txt += "<tr><td>" + obj['Country Or Location'][x] + "</td><td>"+ obj['Total Cases'][x]  +  "</td><td>"+ obj['Total Deaths'][x] + "</td><td>"+ obj['Total Recovered'][x] + "</td><td>"+ obj['Active Cases'][x]+"</td></tr>";
-      }
-      txt += "</table>"    
-      document.getElementById("AffectedCountryTable").innerHTML = txt;
-      Country.pop();
-      TotalCases.pop();
-      ActiveCases.pop();
-      NewCases.pop();
-      TotalDeaths.pop();
-      
-      var plotlayout = {  height: 400,  width: 500};
-      
-      var barchartdataAll = [  { x: Country,    y: TotalCases,    type: 'bar'  }];
-      var barchartdataActive = [  { x: Country,    y: ActiveCases,    type: 'bar'  }];
-      var barchartdataNewCase = [  { x: Country,    y: NewCases,    type: 'bar'  }];
-      var barchartdataTotalDeaths = [  { x: Country,    y: TotalDeaths,    type: 'bar'  }];
-      
-      var piechartdataAll = [  { values:TotalCases ,    labels: Country ,    type: 'pie'  }];
-      var piechartdataActive = [  { values:ActiveCases ,    labels: Country ,    type: 'pie'  }];
-      var piechartdataNewCase = [  { values:NewCases ,    labels: Country ,    type: 'pie'  }];
-      var piechartdataTotalDeaths = [  { values:TotalDeaths ,    labels: Country ,    type: 'pie'  }];
-      
-
-Plotly.newPlot('BarChartAllDiv', barchartdataAll, plotlayout);
-Plotly.newPlot('BarChartActiveDiv', barchartdataActive, plotlayout);
-Plotly.newPlot('BarChartNewCaseDiv', barchartdataNewCase, plotlayout);
-Plotly.newPlot('BarChartTotalDeathsDiv', barchartdataTotalDeaths, plotlayout);
-
-Plotly.newPlot('PieChartAllDiv', piechartdataAll, plotlayout);
-Plotly.newPlot('PieChartActiveDiv', piechartdataActive, plotlayout);
-Plotly.newPlot('PieChartNewCaseDiv', piechartdataNewCase, plotlayout);
-Plotly.newPlot('PieChartTotalDeathsDiv', piechartdataTotalDeaths, plotlayout);
-
-
-
-</script>
-<!-- hitwebcounter Code START -->
-<a href="https://www.hitwebcounter.com" target="_blank">
-<img src="https://hitwebcounter.com/counter/counter.php?page=7206213&style=0038&nbdigits=9&type=page&initCount=0" title="User Stats" Alt="webcounterwebsite"   border="0" >
-</a> 
-</body>
-</html>
-    """
-    return str1 + "'" + str2 + "';" + str3
-
-@app.route("/refresh", methods=['GET'])
-def refresh():
-    #create new thread
-    refreshthread = myThread(2, "ManualRefreshThread")
-    # Start new Threads
-    refreshthread.setDaemon(True)
-    refreshthread.start()
-    return "Refresh completed"
-
+@app.route("/getdata", methods=['GET'])
+def getdata():
+    # this method will fetch the current value of the complete dataframe json and return
+    global dataframejson
+    return Response(dataframejson, 200)
 
 
 
 if __name__ == "__main__":
-    #Initial image and table generation
-    GenerateResources()
-    # create new thread for auto refresh of image and dataframe
+    # create new thread for auto refresh of the dataframe
     thread1 = myThread(1, "AutoLoadThread")
     # Start new Threads
     thread1.setDaemon(True)
