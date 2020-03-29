5e93cd40a1b77ede980f8ad6e3cf2018a494475e
shinilt <shinilstar@gmail.com>
2020-03-29 17:10:40 +0200

    added the mock user agent

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import datetime
 from bs4 import BeautifulSoup
 import matplotlib
 matplotlib.use('Agg')
+import urllib.request
 import matplotlib.pyplot as plt
 import threading
 import seaborn as sns
@@ -41,8 +42,13 @@ def get_new_data_every(period):
         time.sleep(period)
 
 def GenerateResources():
-    URL = 'https://www.worldometers.info/coronavirus/#countries'
-    df = pd.read_html(URL)
+    URL = 'https://www.worldometers.info/coronavirus'
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    request = urllib.request.Request(URL, headers={'User-Agent': user_agent})
+    response = urllib.request.urlopen(request)
+    html = response.read()
+
+    df = pd.read_html(html)
     req_data = requests.get(URL)
     review_soup = BeautifulSoup(req_data.content, 'html.parser')
 
